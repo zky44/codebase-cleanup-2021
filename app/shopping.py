117ca0c27b3f0aba3d656ecc2e59cfe794ea8a2f
MJ Rossetti <s2t2@users.noreply.github.com>
2021-03-22 16:45:18 -0400

    Testing product lookups

--- app/shopping.py
+++ app/shopping.py
@@ -6,75 +6,90 @@ from pandas import read_csv
 def format_usd(my_price):
     return f"${my_price:,.2f}"
 
-
-# READ INVENTORY OF PRODUCTS
-
-products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
-products_df = read_csv(products_filepath)
-products = products_df.to_dict("records")
-
-# CAPTURE PRODUCT SELECTIONS
-
-selected_products = []
-while True:
-    selected_id = input("Please select a product identifier: ")
-    if selected_id.upper() == "DONE":
-        break
+def lookup_product(product_id, all_products):
+    """
+    Params :
+        product_id (str) like "8"
+        all_products (list of dict) each dict should have "id", "name", "department", "aisle", and "price" attributes
+    """
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products):
+        return matching_products[0]
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        if any(matching_products):
-            selected_products.append(matching_products[0])
+        return None
+
+
+if __name__ == '__main__':
+
+
+    # READ INVENTORY OF PRODUCTS
+
+    products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+    products_df = read_csv(products_filepath)
+    products = products_df.to_dict("records")
+
+    # CAPTURE PRODUCT SELECTIONS
+
+    selected_products = []
+    while True:
+        selected_id = input("Please select a product identifier: ")
+        if selected_id.upper() == "DONE":
+            break
         else:
-            print("OOPS, Couldn't find that product. Please try again.")
+            matching_product = lookup_product(selected_id, products)
+            if matching_product:
+                selected_products.append(matching_product)
+            else:
+                print("OOPS, Couldn't find that product. Please try again.")
 
-checkout_at = datetime.now()
+    checkout_at = datetime.now()
 
-subtotal = sum([float(p["price"]) for p in selected_products])
+    subtotal = sum([float(p["price"]) for p in selected_products])
 
-# PRINT RECEIPT
+    # PRINT RECEIPT
 
-print("---------")
-print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
-print("---------")
-for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
-
-print("---------")
-print(f"SUBTOTAL: {format_usd(subtotal)}")
-print(f"TAX: {format_usd(subtotal * 0.0875)}")
-print(f"TOTAL: {format_usd(subtotal * 0.0875 + subtotal)}")
-print("---------")
-print("THANK YOU! PLEASE COME AGAIN SOON!")
-print("---------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# WRITE RECEIPT TO FILE
-
-receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
-receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
-
-with open(receipt_filepath, "w") as receipt_file:
-    receipt_file.write("------------------------------------------")
+    print("---------")
+    print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
+    print("---------")
     for p in selected_products:
-        receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+        print("SELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
 
-    receipt_file.write("\n---------")
-    receipt_file.write(f"\nSUBTOTAL: {subtotal}")
-    receipt_file.write(f"\nTAX: {subtotal * 0.875}")
-    receipt_file.write(f"\nTOTAL: {((subtotal * 0.875) + subtotal)}")
-    receipt_file.write("\n---------")
-    receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
-    receipt_file.write("\n---------")
+    print("---------")
+    print(f"SUBTOTAL: {format_usd(subtotal)}")
+    print(f"TAX: {format_usd(subtotal * 0.0875)}")
+    print(f"TOTAL: {format_usd(subtotal * 0.0875 + subtotal)}")
+    print("---------")
+    print("THANK YOU! PLEASE COME AGAIN SOON!")
+    print("---------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # WRITE RECEIPT TO FILE
+
+    receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
+    receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
+
+    with open(receipt_filepath, "w") as receipt_file:
+        receipt_file.write("------------------------------------------")
+        for p in selected_products:
+            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+
+        receipt_file.write("\n---------")
+        receipt_file.write(f"\nSUBTOTAL: {subtotal}")
+        receipt_file.write(f"\nTAX: {subtotal * 0.875}")
+        receipt_file.write(f"\nTOTAL: {((subtotal * 0.875) + subtotal)}")
+        receipt_file.write("\n---------")
+        receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
+        receipt_file.write("\n---------")
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,26 @@
 
-# TODO: import some code
+import os
+from pandas import read_csv
 
-# TODO: test the code
+from app.shopping import lookup_product
+
+# SETUP
+# todo: consider making this a pytest "fixture"
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+mock_products_df = read_csv(mock_products_filepath)
+mock_products = mock_products_df.to_dict("records")
+
+def test_lookups():
+    # with valid product id, returns the product info:
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name': 'Product 8',
+        'price': 10.0
+    }
+
+    # with invalid product id, returns None:
+    invalid_result = lookup_product("88888888", mock_products)
+    assert invalid_result == None
