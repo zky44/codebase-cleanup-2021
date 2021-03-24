e2f2f5729de0e3b556038a7a3cb2c5c2655aebe7
zky44 <77955005+zky44@users.noreply.github.com>
2021-03-23 22:12:34 -0400

    Refactored valid options in our game

--- app/game.py
+++ app/game.py
@@ -24,26 +24,26 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-#if u == "rock" and c == "rock":
-#    print("It's a tie!")
-#elif u == "rock" and c == "paper":
-#    print("The computer wins")
-#elif u == "rock" and c == "scissors":
-#    print("The user wins")
-#
-#elif u == "paper" and c == "rock":
-#    print("The computer wins")
-#elif u == "paper" and c == "paper":
-#    print("It's a tie!")
-#elif u == "paper" and c == "scissors":
-#    print("The user wins")
-#
-#elif u == "scissors" and c == "rock":
-#    print("The computer wins")
-#elif u == "scissors" and c == "paper":
-#    print("The user wins")
-#elif u == "scissors" and c == "scissors":
-#    print("It's a tie!")
+if u == "rock" and c == "rock":
+    print("It's a tie!")
+elif u == "rock" and c == "paper":
+    print("The computer wins")
+elif u == "rock" and c == "scissors":
+    print("The user wins")
+
+elif u == "paper" and c == "rock":
+    print("The computer wins")
+elif u == "paper" and c == "paper":
+    print("It's a tie!")
+elif u == "paper" and c == "scissors":
+    print("The user wins")
+
+elif u == "scissors" and c == "rock":
+    print("The computer wins")
+elif u == "scissors" and c == "paper":
+    print("The user wins")
+elif u == "scissors" and c == "scissors":
+    print("It's a tie!")
 
 if u == "rock":
     if c == "rock":
